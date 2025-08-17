a1772c543aa6210e61b90ff8a0d605340cc59b2d
MSI\victo <victor5rojaz@hotmail.com>
2025-08-17 17:29:53 -0600

    FEAT:Limpeza,carga de datos del csv y api de clima

--- src/api/ClienteAPI.py
+++ src/api/ClienteAPI.py
@@ -1,2 +1,51 @@
 #api/ → Clase ClienteAPI: realiza peticiones a APIs públicas y transforma los
 #resultados en DataFrames.
+import requests
+import pandas as pd
+
+latitude = 9.9281
+longitude = -84.0907
+variables = "temperature_2m_max,temperature_2m_min,precipitation_sum"
+years = [2020, 2021, 2022, 2023]
+
+all_data = []
+
+for year in years:
+    url = (
+        f"https://archive-api.open-meteo.com/v1/era5?"
+        f"latitude={latitude}&longitude={longitude}"
+        f"&daily={variables}"
+        f"&start_date={year}-01-01&end_date={year}-12-31"
+        f"&timezone=auto"
+    )
+
+    response = requests.get(url)
+    data = response.json()
+    print(f"Datos {year}: ", data.keys())
+
+    df_year = pd.DataFrame(data["daily"])
+    all_data.append(df_year)
+
+#  Concatenar todos los años
+df = pd.concat(all_data, ignore_index=True)
+
+# Convertir fechas
+df["time"] = pd.to_datetime(df["time"])
+df["Año"] = df["time"].dt.year
+df["Mes"] = df["time"].dt.month
+
+#  Promedios mensuales
+df_mensual = (
+    df.groupby(["Año", "Mes"], as_index=False)
+    .agg({
+        "temperature_2m_max": "mean",
+        "temperature_2m_min": "mean",
+        "precipitation_sum": "mean"
+    })
+)
+
+# Guardar a CSV
+df_mensual.to_csv("clima_mensual_2020_2023.csv", index=False)
+
+print("Listo: se generaron promedios mensuales en clima_mensual_2020_2023.csv")
+
--- src/datos/GestorDatos.py
+++ src/datos/GestorDatos.py
@@ -1,2 +1,36 @@
 #datos/ → Clase GestorDatos: encargada de cargar, transformar y exportar
 #archivos CSV, Excel, etc.
+
+import pandas as pd
+
+# 1. Cargar archivo
+
+df = pd.read_csv("C:\Users\victo\OneDrive\Escritorio\New folder\Proyecto_final_grupo-3\data\raw\Datos_Abiertos_ARESEP_Flujo_vehicular_CONAVI_.csv")
+
+# 2. Copia de seguridad para trabajar
+df_clean = df.copy()
+
+#  3. Limpieza básica
+# Quitar espacios extra en nombres de meses
+df_clean["Mes"] = df_clean["Mes"].str.strip()
+
+# Normalizar texto en columnas de tipo object (en este caso Puesto de Peaje)
+df_clean["Puesto de Peaje"] = df_clean["Puesto de Peaje"].str.strip().str.title()
+
+# 4. Eliminar filas donde el puesto de peaje sea "Naranjo"
+df_clean = df_clean[df_clean["Puesto de Peaje"] != "Naranjo"]
+
+# 5. Manejo de valores nulos
+# Eliminar columna con demasiados nulos (Cuatro Ejes)
+if "Cuatro Ejes" in df_clean.columns:
+    df_clean = df_clean.drop(columns=["Cuatro Ejes"])
+
+# Rellenar nulos restantes con 0 (porque son conteos de vehículos)
+df_clean = df_clean.fillna(0)
+
+# 6. Guardar dataset limpio (solo columnas originales)
+output_path = "peajes_clean.csv"
+df_clean.to_csv(output_path, index=False)
+
+print(" Limpieza completada (sin Naranjo, sin columnas extra).")
+print(f"Archivo guardado en: {output_path}")
